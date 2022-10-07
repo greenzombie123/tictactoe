e1f67ee28b735c4ab6856b1c10d194de8f3d021a
Alvin Hart <pageking123@gmail.com>
2022-10-07 14:30:24 +0900

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -331,7 +331,7 @@ const game = (function () {
         const currentPlayer = player1.turn ? player1 : player2;
         element.style.setProperty(
           "background",
-          `no-repeat center/80% url(${"assets/circle.svg"})`
+          `no-repeat center/80% url(${currentPlayer.marker})`
         );
         isMarked = currentPlayer.number;
         checkWinner();
